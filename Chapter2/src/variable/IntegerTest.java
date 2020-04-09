459cde1a02309b847869376e8bd94622bb199c41
LeeJuneHyun <dlwnsgus777@naver.com>
2020-04-09 22:18:38 +0900

    feat: 패스트 캠퍼스 자바

---
+++ Chapter2/src/variable/IntegerTest.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class IntegerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte bs = -127;
+		
+		System.out.println(bs);
+		
+		//int ival = 12345678900;
+		long lval = 12345678900L;
+	}
+
+}
---
+++ Chapter2/src/variable/VariableTest.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age, count;
+		
+		age = 30;
+		
+		System.out.println(age);
+		
+		int numberOfStudent;
+	}
+
+}
