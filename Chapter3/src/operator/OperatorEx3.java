f2979e246db9600053a1ea5ccfae8731c4e7aaaf
LeeJuneHyun <dlwnsgus777@naver.com>
2020-04-11 20:20:08 +0900

    doc: 패스트 캠퍼스 자바 챕터3 ㅁ

--- Chapter3/src/operator/OperatorEx3.java
+++ Chapter3/src/operator/OperatorEx3.java
@@ -8,6 +8,19 @@ public class OperatorEx3 {
 		System.out.println(++score);
 		System.out.println(score++);
 		System.out.println(score);
+		
+		int num1 = 10;
+		int i = 2;
+		
+		boolean value = ((num1 = num1 + 10) < 10) && ((i = i + 2) < 10);
+		System.out.println(value);
+		
+		int num2 = 10;
+		int num3 = 20;
+		
+		int max = (num2 > num3) ? num2 : num3;
+		
+		System.out.println(max);
 	}
 
 }
