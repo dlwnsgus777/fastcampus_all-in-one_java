244b19a0f8aade6ae1ea061a99edf9826eb358e4
LeeJuneHyun <dlwnsgus777@naver.com>
2020-04-12 19:44:29 +0900

    feat: 패스트 캠퍼스 자바 강의 시청

---
+++ Chapter5/src/classpart/StudentTest.java
@@ -0,0 +1,13 @@
+package classpart;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student studentLee = new Student();
+		studentLee.studentName = "이준현";
+		studentLee.address = "인천";
+		
+		studentLee.showStudentInfo();
+	}
+
+}
