ec23c8173d7f8f21bf10add9fcaba6780bd80cd6
LeeJuneHyun <dlwnsgus777@naver.com>
2020-04-19 21:08:42 +0900

    feat: 패스트 캠퍼스 자바 강의 시청

---
+++ Chapter5/src/card/Card.java
@@ -0,0 +1,19 @@
+package card;
+
+public class Card {
+	private int cardNumber;
+	private static int serialNum = 1000;
+	
+	Card() {
+		serialNum++;
+		cardNumber = serialNum;
+	}
+	
+	public int getCardNumber() {
+		return cardNumber;
+	}
+	
+	public void setCardNumber(int cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+}
---
+++ Chapter6/src/array/ArrayTest.java
@@ -0,0 +1,17 @@
+package array;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		int[] arr = new int[10];
+		
+		for (int i = 0, num = 1; i < arr.length; i++, num++) {
+			arr[i] = num;
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
