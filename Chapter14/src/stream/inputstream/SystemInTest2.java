8f72a7b25758dba2c2d7834f885d57ff7222fac3
LeeJuneHyun <dlwnsgus777@naver.com>
2020-05-01 09:27:46 +0900

    feat: 패스트 캠퍼스 자바 강의 시청

---
+++ Chapter14/src/stream/inputstream/SystemInTest2.java
@@ -0,0 +1,19 @@
+package stream.inputstream;
+
+import java.io.IOException;
+
+public class SystemInTest2 {
+
+	public static void main(String[] args) {
+		System.out.println("ют╥б : ");
+		
+		try {
+			int i = System.in.read();
+			System.out.println(i);
+			System.out.println((char)i);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		
+	}
+}
