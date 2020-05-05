f3acad7a11f2106084444ee3084f14740318274b
LeeJuneHyun <dlwnsgus777@naver.com>
2020-05-05 09:56:25 +0900

    feat: 패스트 캠퍼스 자바 강의 시청

---
+++ Chapter15/src/thread/ThreadTest.java
@@ -0,0 +1,56 @@
+package thread;
+
+class MyThread implements Runnable {//extends Thread {
+
+	@Override
+	public void run() {
+		int i;
+		
+		for (i = 0; i <= 200; i++) {
+			System.out.println(i + "\t");
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				System.out.println(e);
+			}
+		}
+	}
+	
+//	public void run() {
+//		int i;
+//		
+//		for (i = 0; i <= 200; i++) {
+//			System.out.println(i + "\t");
+//			
+//			try {
+//				sleep(100);
+//			} catch (InterruptedException e) {
+//				System.out.println(e);
+//			}
+//		}
+//	}
+}
+
+public class ThreadTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("start");
+//		MyThread th1 = new MyThread();
+//		MyThread th2 = new MyThread();
+//		
+//		th1.start();
+//		th2.start();
+		
+		MyThread runner1 = new MyThread();
+		Thread th1 = new Thread(runner1);
+		Thread th2 = new Thread(runner1);
+		
+		th1.start();
+		th2.start();
+		
+		System.out.println("end");
+	}
+
+}
